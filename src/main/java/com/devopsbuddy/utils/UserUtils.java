1bce8ae8bbf19028f95bc6d73371722bed44ef58
Marco Tedone <marco.tedone@gmail.com>
2016-04-16 09:00:25 +0100

    Added first part of the FMP journey

--- src/main/java/com/devopsbuddy/utils/UserUtils.java
+++ src/main/java/com/devopsbuddy/utils/UserUtils.java
@@ -1,6 +1,9 @@
 package com.devopsbuddy.utils;
 
 import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by tedonema on 30/03/2016.
@@ -35,5 +38,29 @@ public class UserUtils {
         user.setProfileImageUrl("https://blabla.images.com/basicuser");
 
         return user;
+    }
+
+    /**
+     * Builds and returns the URL to reset the user password.
+     * @param request The Http Servlet Request
+     * @param userId The user id
+     * @param token The token
+     * @return the URL to reset the user password.
+     */
+    public static String createPasswordResetUrl(HttpServletRequest request, long userId, String token) {
+        String passwordResetUrl =
+                request.getScheme() +
+                        "://" +
+                        request.getServerName() +
+                        ":" +
+                        request.getServerPort() +
+                        request.getContextPath() +
+                        ForgotMyPasswordController.CHANGE_PASSWORD_PATH +
+                        "?id=" +
+                        userId +
+                        "&token=" +
+                        token;
+
+        return passwordResetUrl;
     }
 }
