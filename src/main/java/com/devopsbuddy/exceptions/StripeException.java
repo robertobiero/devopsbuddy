46d8d9beed6fe872fd5282f4578f1123bc137ce8
Marco Tedone <marco.tedone@gmail.com>
2016-05-14 20:35:18 +0100

    Implemented exception handling for Subscription journey

--- src/main/java/com/devopsbuddy/exceptions/StripeException.java
+++ src/main/java/com/devopsbuddy/exceptions/StripeException.java
@@ -4,4 +4,9 @@ package com.devopsbuddy.exceptions;
  * Created by tedonema on 13/05/2016.
  */
 public class StripeException extends RuntimeException {
+
+    public StripeException(Throwable e) {
+        super(e);
+    }
+
 }
